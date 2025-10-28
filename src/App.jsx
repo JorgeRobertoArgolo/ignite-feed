81f6d0d66aeae4c2b2b18a41720b0b1029a646cc
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2025-10-28 06:45:21 -0300

    feat:  criando componente de header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello world
+      <Header />
     </>
   )
 }
