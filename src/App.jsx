bbd8d3167f33c8c47f561b9dce7a0819d272b4f5
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2025-10-28 08:54:02 -0300

    feat: componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import './global.css'
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main />
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from "@phosphor-icons/react";
+
+export function Sidebar () {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGV2ZWxvcGVyfGVufDB8fDB8fHww&auto=format&fit=crop&q=50&w=500" 
+                alt="Capa do perfil" 
+            />
+
+            <div className={styles.profile}>
+
+            <img 
+                className={styles.avatar} 
+                src="https://avatars.githubusercontent.com/u/160530109?v=4" 
+            />
+
+                <strong>Jorge Roberto</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
